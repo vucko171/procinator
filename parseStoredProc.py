eaac1b85b91810647ade37d46df59419ea131820
vucko171 <57326626+vucko171@users.noreply.github.com>
2020-10-07 16:27:26 -0700

    init

---
+++ parseStoredProc.py
@@ -0,0 +1,39 @@
+import re, pyperclip, time
+def parse(data):
+    data=data.split("\n")
+    name=""
+    ret=""
+    params=[]
+    for line in data:
+        if name=="":
+            nameArr = re.findall(r'\.CommandText\s*=\s*"JAGORA\.(.*?)"',line,flags=re.I)
+            if len(nameArr):
+                name=nameArr[0]
+        paramsArr=re.findall(r'\.CreateParameter\((.*?),.*adParamInput.*,(.*?)\)\s*$', line, flags=re.I)
+        if len(paramsArr):
+            params.append(paramsArr[0])
+    ret+="try {\n"
+    ret+="  const sp = new CallStoredProcedure(connection);\n"
+    ret+="  sp.commandText = 'JAGORA."+name+"';\n"
+    for param in params:
+        ret+="  sp.createParameter("+param[0]+", "+param[1]+");\n"
+    ret+="  const temp = await sp.execute(false);\n"
+    ret+="} catch (error) {\n"
+    ret+="  connection.rollback();\n"
+    ret+="  throw new Error(error);\n"
+    ret+="  // do something\n"
+    ret+="}\n"
+    return ret
+
+    
+if __name__=="__main__":
+    ptext=""
+    while(1):
+        text=pyperclip.paste()
+        if text!=ptext:
+            if "CommandText" in text:
+                ret=parse(text)
+                pyperclip.copy(ret)
+                text=ret
+            ptext=text
+        time.sleep(0.1)
